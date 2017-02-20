482ab851aeebbf6a340c9630c5deee180045ed1a
陈广文(399399) <289929553@qq.com>
2017-02-20 18:37:34 +0800

    增加根据终端进行分组显示

--- src/components/GroupSelect.js
+++ src/components/GroupSelect.js
@@ -11,16 +11,31 @@ export default class GroupSelect extends React.Component{
     this.render = this.render.bind(this);
     this.groupByErrorKey = this.groupByErrorKey.bind(this);
     this.groupByPageUrl = this.groupByPageUrl.bind(this);
-
-    var datas = [];
+    this.groupByClient = this.groupByClient.bind(this);
+    var datas = [],that = this;
     _.forEach(this.props.datas,function(data){
         var ex_msg = JSON.parse(data['ex_msg']);
+        ex_msg['client'] = that.getBowerInfo(ex_msg.userAgent);
         datas.push(ex_msg);
     })
     this.state = {
       groupType: 'key',
       datas:datas
     };
+  }
+  getBowerInfo(userAgent){
+    if(userAgent.indexOf('Android')>-1|| userAgent.indexOf("Linux") > -1){
+      return 'Android';
+    }
+    if(userAgent.indexOf('iPhone')>-1){
+      return 'iPhone';
+    }
+    if(userAgent.indexOf('iPad')>-1){
+      return 'iPad';
+    }
+    if(userAgent.indexOf('Windows')>-1){
+      return 'Windows';
+    }
   }
   componentDidMount() {
     this.groupByErrorKey();
@@ -50,6 +65,18 @@ export default class GroupSelect extends React.Component{
       groupType: 'url'
     })
   }
+  /**
+   * 根据错误code分组
+   */
+  groupByClient() {
+    var groupDatas = _.groupBy(this.state.datas,function(d){
+        return d.client;
+    });
+    this.afterGroup(groupDatas);
+    this.setState({
+      groupType: 'client'
+    })
+  }
   afterGroup(groupDatas){
     var items = [];
     _.mapKeys(groupDatas, function(value, key) {
@@ -64,8 +91,10 @@ export default class GroupSelect extends React.Component{
   render() {
     return (
            <div className="btn-group" role="group" aria-label="...">
-              <button type="button" className={this.state.groupType == 'key' ? 'btn btn-primary' : 'btn'} onClick={this.groupByErrorKey}>根据错误消息</button>
-              <button type="button" className={this.state.groupType == 'url' ? 'btn btn-primary' : 'btn'} onClick={this.groupByPageUrl}>根据页面URL</button>
+              <button type="button" className={this.state.groupType == 'key' ? 'btn btn-primary' : 'btn btn-default'} onClick={this.groupByErrorKey}>根据错误消息</button>
+              <button type="button" className={this.state.groupType == 'url' ? 'btn btn-primary' : 'btn btn-default'} onClick={this.groupByPageUrl}>根据页面URL</button>
+              <button type="button" className={this.state.groupType == 'client' ? 'btn btn-primary' : 'btn btn-default'} onClick={this.groupByClient}>根据终端</button>
+              
             </div>
             
         );
